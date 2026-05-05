9c777fa5597b35eaaa12eeb860ccf87f4f603667
Lemper29 <buumhalal@gmail.com>
2026-05-05 22:11:33 +0300

    feat: passed the structure and terminal management

--- main.c
+++ main.c
@@ -31,12 +31,11 @@ int main(int argc, char **argv) {
     struct memory chunk = { 0 }; 
 
     char url[1000];
-    snprintf(url, sizeof(url),"https://en.wikipedia.org/w/api.php?action=query&titles=%s&prop=extracts&explaintext=1&format=json", argv[1]);
+    snprintf(url, sizeof(url),"https://eu.wikipedia.org/w/api.php?action=query&titles=%s&prop=extracts&explaintext=1&format=json", argv[1]);
 
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *handler = curl_easy_init();
     if (handler) {
-        
         curl_easy_setopt(handler, CURLOPT_URL, url);
         curl_easy_setopt(handler, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
         curl_easy_setopt(handler, CURLOPT_WRITEFUNCTION, write_callback);
@@ -45,37 +44,29 @@ int main(int argc, char **argv) {
         CURLcode response = curl_easy_perform(handler);
         if (response == 0) {
             cJSON *json = cJSON_Parse(chunk.response);
-            if (json == NULL) {
-                const char *error_ptr = cJSON_GetErrorPtr();
-                if (error_ptr != NULL) {
-                    printf("Error: %s\n", error_ptr);
-                }
-                cJSON_Delete(json);
-                return 1;
-            }
-            cJSON *query = cJSON_GetObjectItemCaseSensitive(json, "query");
-            if (query == NULL) {
-                printf("No 'query' field\n");
-                cJSON_Delete(json);
-                return 1;
-            }
-
-            cJSON *pages = cJSON_GetObjectItemCaseSensitive(query, "pages");
-            if (pages == NULL) {
-                printf("No 'pages' field\n");
-                cJSON_Delete(json);
-                return 1;
-            }
-
-            const cJSON *page = NULL;
-            cJSON_ArrayForEach(page, pages) {
-                cJSON *extract = cJSON_GetObjectItemCaseSensitive(page, "extract");
-                if (extract != NULL && cJSON_IsString(extract)) {
-                    printf("%s\n", extract->valuestring);
-                    break;  
+            if (json) {
+                cJSON *query = cJSON_GetObjectItemCaseSensitive(json, "query");
+                if (query) {
+                    cJSON *pages = cJSON_GetObjectItemCaseSensitive(query, "pages");
+                    if (pages) {
+                        const cJSON *page = NULL;
+                        cJSON_ArrayForEach(page, pages) {
+                        cJSON *extract = cJSON_GetObjectItemCaseSensitive(page, "extract");
+                        if (extract != NULL && cJSON_IsString(extract)) {
+                            FILE *less = popen("less -R", "w");
+                            if (less) {
+                                fprintf(less, "%s\n", extract->valuestring);
+                                pclose(less);
+                            } else {
+                                printf("%s\n", extract->valuestring);
+                            }
+                            break;
+                        }
+                    }
                 }
             }
             cJSON_Delete(json);
+        }
         } else {
             printf("failed req. Code: %d\n", response);
         }
