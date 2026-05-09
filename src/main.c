cf01c1f54a9f6e9f294929dd06852e3ad3ec3554
Lemper29 <buumhalal@gmail.com>
2026-05-09 13:47:54 +0300

    fix: remove duplicate write_callback from main.c

--- src/main.c
+++ src/main.c
@@ -15,22 +15,6 @@ struct memory {
     size_t size;
 };
 
-static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-    size_t realsize = size * nmemb;
-    struct memory *mem = (struct memory *)userp;
-    if (!mem) return 0;
-
-    char *ptr = realloc(mem->data, mem->size + realsize + 1);
-    if (!ptr) return 0;
-
-    mem->data = ptr;
-    memcpy(mem->data + mem->size, contents, realsize);
-    mem->size += realsize;
-    mem->data[mem->size] = '\0';
-    return realsize;
-}
-
 int main(int argc, char **argv)
 {
     if (argc != 2) {
